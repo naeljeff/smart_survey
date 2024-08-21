5965cab2b9abf3361b688ff467b2ddd8da2514e4
naeljeff <naelj15@gmail.com>
2024-08-21 16:51:09 +0700

    added notification page

---
+++ src/components/layoutComponent/Notification/organism/NotificationBody.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const NotificationBody = () => {
+  return (
+    <View>
+      <Text>lorem2000</Text>
+    </View>
+  )
+}
+
+export default NotificationBody
--- src/screens/NotificationPage.tsx
+++ src/screens/NotificationPage.tsx
@@ -1,21 +1,44 @@
-import {Text, View} from 'react-native';
+import {Text, View, ScrollView} from 'react-native';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 import {RootStackParamList} from '../routes/StackNavigator';
 import NavigationHeader from '../components/reusableComponent/Header/NavigationHeader';
+import NotificationBody from '../components/layoutComponent/Notification/organism/NotificationBody';
 
 const NotificationPage = () => {
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   return (
     <View className="flex-1 bg-white">
+      {/* Header */}
       <NavigationHeader
         title={'Notifications'}
         onPress={() => navigation.goBack()}
         onRefresh={() => {}}
       />
+
+      {/* Body */}
+      <View className="flex-1 flex flex-col bg-white">
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            flexGrow: 1,
+            justifyContent: 'flex-end',
+            paddingTop: 100,
+          }}
+          // refreshControl={
+          //   <RefreshControl
+          //     refreshing={refreshing}
+          //     onRefresh={onRefresh}
+          //     colors={['#00bfff']}
+          //   />
+          // }
+          >
+            <NotificationBody />
+        </ScrollView>
+      </View>
     </View>
   );
 };
